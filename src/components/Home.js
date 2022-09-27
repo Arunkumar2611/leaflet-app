82c3fccea7e45b0675326fb133e2668ccc5c5d61
arun kumar <arunkumar923gole@gmail.com>
2022-09-27 22:32:35 +0530

    created a tablular format dataset of volcanos

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TableView from './TableView'
+
+const Home = () => {
+  return (
+    <>
+    <div>Home</div>
+    {/* <TableView /> */}
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => (
+  <nav>
+    <section>
+      <Link to="/">Home</Link>
+      <Link to="/dataset">Dataset</Link>
+      <Link to="/tableview">Table View</Link>
+    </section>
+  </nav>
+)
+
+export default Navbar
