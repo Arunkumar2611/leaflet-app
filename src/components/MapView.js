1917215b9f4cfd0e52aa1707620e3a7619f91f0c
arun kumar <arunkumar923gole@gmail.com>
2022-10-01 19:28:36 +0530

    adding some code

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import { connect } from "react-redux";
 import { fetchData } from "../action/actionVolcano";
-import { MapContainer, TileLayer, Marker, Popup, LayersControl } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, LayersControl, LayerGroup, Circle } from 'react-leaflet'
 import ReactLeafletGoogleLayer from 'react-leaflet-google-layer';
 
 const MapView = (props) => {
   const position = [20.5937, 78.9629]
+  const center = [20.5937, 78.9629]
 
   useEffect(() => {
     const { dispatch } = props;
@@ -35,8 +36,29 @@ const MapView = (props) => {
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           />
         </LayersControl.BaseLayer>
+
+        <LayersControl.Overlay checked name="Volcano Data">
+          <LayerGroup>
+            {
+              items.map((row) =>
+                <Marker
+                  key={row.properties.VolcanoID}
+                  position={[
+                    row.geometry.coordinates[1],
+                    row.geometry.coordinates[0],
+                  ]}>
+                  <Popup>
+                    <p><b>Volcano Name: </b> {row.properties.V_Name}</p>
+                    <p><b>Country: </b> {row.properties.Country}</p>
+                    <p><b>Region: </b> {row.properties.Region}</p>
+                  </Popup>
+                </Marker>
+              )
+            }
+          </LayerGroup>
+        </LayersControl.Overlay>
       </LayersControl>
-      {
+      {/* {
         items.map((row) =>
           <Marker
             key={row.properties.VolcanoID}
@@ -51,7 +73,7 @@ const MapView = (props) => {
             </Popup>
           </Marker>
         )
-      }
+      } */}
     </MapContainer>
   )
 }
